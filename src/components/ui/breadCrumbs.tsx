14f4d128d128e07fc19b33f95b40578d29d338e2
@mohammed0ibrahim <mohammeedd095959225@gmail.com>
2024-02-07 06:42:47 -0800

    add bread crumbs

--- src/components/ui/breadCrumbs.tsx
+++ src/components/ui/breadCrumbs.tsx
@@ -5,41 +5,46 @@ interface BreadCrumbsProps {
   label: string;
   Link: string;
 }
-[];
 
-const BreadCrumbs: React.FunctionComponent<{ data: BreadCrumbsProps }> = ({
-  link,
+const BreadCrumbs: React.FunctionComponent<{ data: BreadCrumbsProps[] }> = ({
+  data,
 }) => {
   return (
     <div className="h-7 flex gap-2 items-center">
       <Link to="" className="w-5 h-5 flex justify-center items-center">
         <img
-          src={home}
+          src={"/svg/global/home-line.svg"}
           alt="home"
           className="flex just ify-center items-center"
         />
       </Link>
       <img
-        src={arrow}
-        alt="arrow"
+        src={"/public/svg/global/chevron-right.svg"}
+        alt="right-arrow"
         className="flex justify-center items-center"
       />
-      {links.map((link, idx) => {
+      {data.map((link, index) => {
         return (
-          <div className="flex h-7 gap-2 items-center" key={idx}>
+          <div className="flex h-7 gap-2 items-center" key={index}>
             <Link
-              href={link.Link}
+              to={link.Link}
               className="h-5 flex justify-center items-center w-fit"
             >
               <Button
                 className={`shadow-none border-none rounded-md py-1 px-2 ${
-                  idx + 1 === links.length ? "bg-gray-50" : ""
+                  index + 1 === data.length ? "bg-gray-50" : ""
                 } text-gray-700 flex items-center justify-center font-semibold leading-5 text-Text-sm`}
               >
                 {link.label}
               </Button>
             </Link>
-            {idx + 1 !== links.length && <img src={arrow} alt="arrow" />}
+            {index + 1 !== data.length && (
+              <img
+                src={"/public/svg/global/chevron-right.svg"}
+                alt="right-arrow"
+                className="flex justify-center items-center"
+              />
+            )}
           </div>
         );
       })}

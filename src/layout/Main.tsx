d253072a5611871c6ade12286d65f700e6300dae
joudyjrrr <joudyjrrr@gmail.com>
2024-02-07 16:33:29 +0300

    first commit

--- src/layout/Main.tsx
+++ src/layout/Main.tsx
@@ -3,7 +3,7 @@ import { Outlet } from "react-router-dom";
 
 const Main = ({}) => {
   return (
-    <main className="">
+    <main className="col-span-2">
       <Outlet />
     </main>
   );
--- src/pages/dashboard/Overview.tsx
+++ src/pages/dashboard/Overview.tsx
@@ -2,7 +2,7 @@ import React from "react";
 
 const Overview = () => {
   return (
-    <div className="bg-gray-50 bg-cover bg-center  w-full px-[2.5rem] py-12  m-0 col-span-2">
+    <div className="bg-gray-50 bg-cover bg-center  w-full px-[2.5rem] py-12  m-0 ">
 
     </div>
   );
